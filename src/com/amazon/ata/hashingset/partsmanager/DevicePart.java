f092ef7cd9cbc0557136d6a72ff5786cc3540bae
Santiago Coria <santiago_coria@hotmail.com>
2024-05-14 23:10:25 -0300

    Guided Project

--- src/com/amazon/ata/hashingset/partsmanager/DevicePart.java
+++ src/com/amazon/ata/hashingset/partsmanager/DevicePart.java
@@ -1,9 +1,10 @@
 package com.amazon.ata.hashingset.partsmanager;
 
 import java.util.List;
+import java.util.Objects;
 
-public class DevicePart {
-    private String manufacturer;
+public class    DevicePart {
+    private String manufacturer;  //read - only (not setters) inmutable
     private String manufacturersPartNumber;
     private List<AmazonDevice> devicesUsedIn;
 
@@ -11,6 +12,21 @@ public class DevicePart {
         this.manufacturer = manufacturer;
         this.manufacturersPartNumber = manufacturersPartNumber;
         this.devicesUsedIn = devicesUsedIn;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        System.out.println("in teh.equals() for Device Part");
+        if (this == o) return true;
+        if (!(o instanceof DevicePart)) return false;
+        DevicePart that = (DevicePart) o;
+        return Objects.equals(getManufacturer(), that.getManufacturer()) && Objects.equals(getManufacturersPartNumber(), that.getManufacturersPartNumber());
+    }
+
+    @Override
+    public int hashCode() {
+        System.out.println("in teh.hashCode() for Device Part");
+        return Objects.hash(getManufacturer(), getManufacturersPartNumber());
     }
 
     public String getManufacturer() {
